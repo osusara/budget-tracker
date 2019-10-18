f5ca68bace440f6e08606ea0f28ed99713acff53
Osusara Kammalawatta <osusarak96@gmail.com>
2019-10-18 21:55:43 +0530

    Add feature to save user information in firestore as well in signup

--- src/components/userManagement/SignUp.js
+++ src/components/userManagement/SignUp.js
@@ -6,18 +6,21 @@ import Error from "../warnings/Error";
 class SignUp extends Component {
   constructor(props) {
     super(props);
-    
+
     this.login = this.login.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.signup = this.signup.bind(this);
 
     this.state = {
+      name: "",
       email: "",
       password: "",
-      errors: "",
+      errors: ""
       // isSignUp: false
     };
   }
+
+  db = firebaseApp.firestore();
 
   handleChange(e) {
     this.setState({ [e.target.name]: e.target.value });
@@ -31,11 +34,13 @@ class SignUp extends Component {
 
   signup(e) {
     e.preventDefault();
+
     firebaseApp
       .auth()
       .createUserWithEmailAndPassword(this.state.email, this.state.password)
       .then(u => {
         console.log(u);
+        this.addUserData(this.state.name, this.state.email, u.user.uid);
       })
       .catch(error => {
         // console.log(error);
@@ -43,15 +48,44 @@ class SignUp extends Component {
       });
   }
 
+  addUserData = (name, email, uid) => {
+    this.db
+      .collection("users").doc(uid)
+      .set({
+        name,
+        email
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(function(error) {
+        console.error("Error adding document: ", error);
+      });
+  };
+
   render() {
     return (
       <div className="container py-5 my-5">
-        <div className="col-md-6 col-sm-10 col-xs-11 mx-auto my-5 py-5 text-center">
-          <div className="card py-3 px-2" style={loginCard}>
-            <h2 className="card-title">Signup</h2>
+        <div className="col-md-5 col-sm-8 col-xs-11 mx-auto my-5 py-5 text-center">
+          <div className="card shadow py-3 px-2" style={loginCard}>
+            <h3 className="card-title py-2" style={headerText}>
+              Create Account
+            </h3>
             <div className="card-body">
               {this.state.errors ? <Error message={this.state.errors} /> : null}
               <form>
+                <div className="form-group">
+                  <input
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                    type="text"
+                    name="name"
+                    className="form-control text-center"
+                    style={input}
+                    placeholder="Full Name"
+                  />
+                </div>
+                <hr className="mx-auto" style={divider} />
                 <div className="form-group">
                   <input
                     value={this.state.email}
@@ -59,13 +93,9 @@ class SignUp extends Component {
                     type="email"
                     name="email"
                     className="form-control text-center"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
+                    style={input}
                     placeholder="Email"
                   />
-                  <small id="emailHelp" className="form-text">
-                    We'll never share your email with anyone else.
-                  </small>
                 </div>
                 <div className="form-group">
                   <input
@@ -74,21 +104,24 @@ class SignUp extends Component {
                     type="password"
                     name="password"
                     className="form-control text-center"
-                    id="exampleInputPassword1"
+                    style={input}
                     placeholder="Password"
                   />
                 </div>
                 <button
                   type="submit"
-                  onClick={this.login}
+                  onClick={this.signup}
                   className="btn btn-primary"
+                  style={button}
                 >
-                  Login
+                  Sign Up
                 </button>
-                <label className="text-center px-2">or</label>
-                <button onClick={this.signup} className="btn btn-secondary">
-                  Signup
-                </button>
+                <p className="text-center px-2">
+                  Already have an account?{" "}
+                  <label style={link} onClick={this.login}>
+                    Login
+                  </label>
+                </p>
               </form>
             </div>
           </div>
@@ -99,9 +132,39 @@ class SignUp extends Component {
 }
 
 const loginCard = {
-  backgroundColor: "#DDD",
+  backgroundImage: "linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)",
   color: "#444",
-  border: "none"
+  border: "none",
+  borderRadius: "15px",
+  boxShadow: "1px 2px 5px #DDD",
+};
+
+const headerText = {
+  color: "#555",
+};
+
+const input = {
+  border: "none",
+  borderRadius: "100px",
+  width: "90%",
+  margin: "auto",
+};
+
+const divider = {
+  width: "40%",
+}
+
+const button = {
+  backgroundImage: "linear-gradient(135deg, #667eea 0%, #764ba2 100%)",
+  border: "none",
+  borderRadius: "100px",
+  width: "45%",
+  margin: "2% 0 4% 0",
+};
+
+const link = {
+  color: "#764ba2",
+  cursor: "pointer"
 };
 
 export default SignUp
