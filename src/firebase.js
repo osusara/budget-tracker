64468cf9eb4952064bc5e86c928cd2c2ed9ecdb9
Osusara Kammalawatta <30489601+osusara@users.noreply.github.com>
2020-04-17 20:56:31 +0530

    hide firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyD14W3gHhN5-x9KItQu5_PhUksXLQrUjhM",
-  authDomain: "ossa-budget-cal.firebaseapp.com",
-  databaseURL: "https://ossa-budget-cal.firebaseio.com",
-  projectId: "ossa-budget-cal",
-  storageBucket: "ossa-budget-cal.appspot.com",
-  messagingSenderId: "723334135682",
-  appId: "1:723334135682:web:4e16dea22e10e39b0a199b",
-  measurementId: "G-63GDZE716J",
+  apiKey: <API_KEY>,
+  authDomain: <AUTH_DOMAIN>,
+  databaseURL: <DATABASE_URL>,
+  projectId: <PROJECT_ID>,
+  storageBucket: <STORAGE_BUCKET>,
+  messagingSenderId: <MESSAGING_SENDER_ID>,
+  appId: <APP_ID>,
+  measurementId: <MEASUREMENT_ID>,
 });
-
-//const db = firebaseApp.firestore();
 
 export default firebaseApp;
